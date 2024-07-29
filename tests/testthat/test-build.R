f38a6a8c97a4fe71365c3c3ef99098571511cf31
Jason Taylor <jmtaylor@fredhutch.org>
2024-07-29 13:50:39 -0700

    fix tests

--- tests/testthat/test-build.R
+++ tests/testthat/test-build.R
@@ -1,53 +1,8 @@
 tdir <- getPkgDir()
 pkgn <- "testPkg"
-path <- file.path(tdir, pkgn)
-
-DPR2::dpr_init(
-  tdir,
-  yaml=DPR2::dpr_yaml_init(process_on_build=c("01.R", "02.R")),
-  desc=DPR2::dpr_description_init(Package=pkgn)
-)
-
-## test that libraries can be loaded and accessed
-writeLines(
-  c(
-    "library(yaml)",
-    "dfm <- data.frame(x=1:10, y=LETTERS[1:10])",
-    "yml <- as.yaml(df)",
-    "save(dfm, file='data/mydataframe.rda')",
-    "save(yml, file='data/myyaml.rda')"
-  ),
-  file.path(path, "processing/01.R")
-)
-
-writeLines(
-  c(
-    "dat <- matrix(1:16, nrow=4)",
-    "save(dat, file='data/mymatrix.rda')"
-  ),
-  file.path(path, "processing/02.R")
-)
-
-## a processing script that accesses the datapackager.yml
-writeLines(
-  c(
-    "dat <- as.list(LETTERS)",
-    "save(dat, file=file.path(DPR2::dpr_yaml_get()$data_directory, 'letters.rda'))"
-  ),
-  file.path(path, "processing/A1.R")
-)
-
-## check if environment is shared
-writeLines(
-  c(
-    "save(dat, file=file.path(DPR2::dpr_yaml_get()$data_directory, 'dat.rda'))"
-  ),
-  file.path(path, "processing/S1.R")
-)
 
 testthat::test_that("checking package build", {
 
-  pkgn <- "testPkg"
   path <- file.path(tdir, pkgn)
   initPkg(tdir, pkgn, list(renv_init = FALSE))
 
@@ -94,6 +49,12 @@ testthat::test_that("checking package build", {
   ## no variables should be in calling environment
   expect_false(exists("chkvar", environment()))
 
+  ## check yaml validation
+  expect_error(
+    dpr_build(path, render_env_mode = "not valid"),
+    "Invalid `render_env_mode` yaml value used. Please one of these:"
+  )
+  
   unlink(file.path(tdir, pkgn), recursive = TRUE)
 
 })
@@ -104,13 +65,6 @@ testthat::test_that("checking package render",{
   ## check that dpr_render does not build package
   ## check that working env contains render generated var names
 
-})
-
-testthat::test_that("checking yaml validation", {
-  expect_error(
-    dpr_build(path, render_env_mode = "not valid"),
-    "Invalid `render_env_mode` yaml value used. Please one of these:"
-  )
 })
 
 cleanup(tdir)
