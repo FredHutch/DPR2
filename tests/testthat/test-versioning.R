a5a938ae66810c176b7ed16a992190cbec0c07c7
Jason Taylor <jmtaylor@fredhutch.org>
2024-08-30 14:25:35 -0700

    move versioning test to versioning test file, add tests

---
+++ tests/testthat/test-versioning.R
@@ -0,0 +1,109 @@
+## check defaulting to data_digest when git is not used in the repository
+testthat::test_that("check dpr_hash against git2r hash", {
+  path <- "setup.R"
+  expect_true(
+    dpr_hash_file(path) == 
+      git2r::hashfile(path)
+  )
+})
+
+## dpr_data_hashes and dpr_data_digest
+testthat::test_that("checking package data hashes report", {
+
+  tdir <- getPkgDir()
+  pkgn <- "testPkg"
+  path <- file.path(tdir, pkgn)
+
+  initPkg(
+    tdir,
+    pkgn
+  )
+
+  dpr_build(path)
+
+  script <- file.path(path, "processing/01.R")
+  writeLines(gsub("y=", "z=", readLines(script)), script)
+  
+  dpr_render(path)
+  
+  expect_true(
+    nrow(dpr_data_hashes(path)) == 5
+  )
+  
+  expect_true(
+    nrow(dpr_data_digest(path)) == 5
+  )
+  
+  comp <- dpr_compare_data_digest(path)
+  expect_true(!comp[comp$name == "mydataframe.rda", "same"])
+  expect_true(comp[comp$name == "mydataframe_rmd.rda", "same"])
+
+  expect_warning({    
+    dhst <- dpr_data_history(path)
+    expect_true(
+      identical(dhst, comp)
+    )
+  })
+
+  expect_true(
+    all(
+        names(comp) == c("name", "data_hash", "data_digest_hash", "same")
+    )
+  )
+  
+  unlink(path, recursive = TRUE)
+  cleanup(tdir)
+  
+})
+
+## dpr_data_digest
+testthat::test_that("checking package data history with git", {
+
+  tdir <- getPkgDir()
+  pkgn <- "testPkg"
+  path <- file.path(tdir, pkgn)
+
+  initPkg(
+    tdir,
+    pkgn
+  )
+
+  dpr_build(path)
+  
+  git2r::init(path = path, branch="main")
+  git2r::add(repo = path, path = ".")
+  git2r::commit(repo = path, message="commit 1")
+
+  script <- file.path(path, "processing/01.R")
+  writeLines(gsub("y=", "z=", readLines(script)), script)
+  dpr_build(path)
+
+  git2r::add(repo = path, path = ".")
+  git2r::commit(repo = path, message="commit 2")
+  
+  script <- file.path(path, "processing/01.R")
+  writeLines(gsub("z=", "y=", readLines(script)), script)
+  dpr_build(path)
+
+  git2r::add(repo = path, path = ".")
+  git2r::commit(repo = path, message="commit 3")
+  
+  dataHistory <- dpr_data_history(path=path, include_checksums=TRUE)
+
+  expect_equal( ncol(dataHistory), 6 )
+  expect_equal( nrow(dataHistory), 7 )
+  expect_true( all(row.names(dataHistory) == 1:nrow(dataHistory)) )
+  expect_equal(
+      length(
+          unique(
+            dataHistory[
+              dataHistory$object_md5 == names(which(table(dataHistory$object_md5) > 1)),"object_md5"
+            ]
+          )
+        ), 1
+      )
+
+  unlink(path, recursive = TRUE)
+  cleanup(tdir)
+  
+})
