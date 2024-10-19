c0f41b39cc087c3db2d4252a5b19747a9f57cb47
Jason Taylor <jmtaylor@fredhutch.org>
2024-10-19 08:48:23 -0700

    vectorizing and refactoring history and recall functions

--- R/version.R
+++ R/version.R
@@ -15,7 +15,7 @@ dpr_update_data_digest <- function(path=".", yml){
   unlink(list.files(dig, full.names = T))
   
   for(d in rda)
-    write(dpr_hash_file(d), file.path(dig, paste0(basename(d), "_")))
+    write(dpr_hash_files(d), file.path(dig, paste0(basename(d), "_")))
 
 }
 
@@ -28,26 +28,10 @@ dpr_update_data_digest <- function(path=".", yml){
 dpr_list_rda <- function(path){
   sort(
     list.files(
-      file.path(path, dpr_yaml_get(path)$data_directory),
-      "\\.[Rr][Dd][Aa]$",
-      full.names = T
-    )
-  )
-}
-
-##' Private. Get md5 checksum of object once loaded from a path.
-##'
-##' @title dpr_checksum_data_objects
-##' @param path a path to an rda file
-##' @return a string of an md5 checksum of an object loaded from a path
-##' @author jmtaylor
-dpr_checksum_data_objects <- function(path){
-  env <- new.env(parent=emptyenv())
-  load(path, env)
-  return(
-    digest::digest(
-      get(ls(envir=env), envir=env),
-      algo="md5"
+      file.path(path, "data"),
+      "\\.rda$",
+      full.names = TRUE,
+      ignore.case = TRUE
     )
   )
 }
@@ -73,45 +57,67 @@ dpr_check_git <- function(path){
 
 ##' Return hash of file at the path provided. Hash is equivalent to the git blob hash of the file.
 ##'
-##' This function will return a git style sha1 hash of a file found at the path argument. Git style
-##' hashing pads the front of the file with "blob" the length of the file in bytes, a null
-##' character, and then the binary content of the file itself. This style of hashing is used here to
-##' ensure that data objects created across objects that have the same blob can be compared whether
-##' the package is under version control or not, or packages that are put under version control at
+##' This function will return a git style sha1 hash of a file found at the path argument. Git
+##' style hashing pads the front of the file with "blob" the length of the file in bytes, a null
+##' character, and then the binary content of the file itself. This style of hashing is used here
+##' to ensure that data objects created that have the same blob can be compared whether the
+##' package is under version control or not, or packages that are put under version control at
 ##' future dates can compare objects with versions of the package from before version control was
 ##' applied.
+##'
 ##' @title dpr_hash_file
-##' @param path path of file to hash
+##' @param paths path of file to hash
 ##' @return a character string of the SHA1 hash of the file using git style hash padding.
 ##' @author jmtaylor
 ##' @export
-dpr_hash_file <- function(path){
-  digest::digest(
-    c(
-      charToRaw(paste0("blob ", file.info(path)$size)),
-      as.raw(0),
-      readBin(path, "raw", file.info(path)$size)
-    ), algo = "sha1", serialize = F
+dpr_hash_files <- function(paths){
+  if(!all(file.exists(paths)))
+    stop("Cannot hash some paths because they don't exists: ", paste(paths[!file.exists(paths)], collapse = ","))
+  return(
+    vapply(paths, function(path) {
+      digest::digest(
+        c(
+          charToRaw(paste0("blob ", file.info(path)$size)),
+          as.raw(0),
+          readBin(path, "raw", file.info(path)$size)
+        ), algo = "sha1", serialize = F
+      )
+    }, "", USE.NAMES = FALSE)
   )
 }
 
-##' Return the current hashes recorded to the data digest directory during the last build. 
+##' Private. Reads a data digest source, validates it's contents, and returns a list.
+##'
+##' @title dpr_validate_data_digest_source
+##' @param path path to data package
+##' @return a list of a data digest source 
+##' @author jmtaylor
+dpr_validate_data_digest_source <- function(path){
+  data_digest <- list.files(
+    file.path(path, dpr_yaml_get(path)$data_digest_directory),
+    full.names = TRUE
+  )
+  if(length(data_digest) == 0)
+    stop("No digest files found. Has any data been added to the data package yet?")
+  if(!all(grepl("\\.rda_$", data_digest, ignore.case = TRUE)))
+    stop("Invalid file in `data_digest` directory. Should only contain files with an `.rda_` extension.")
+  return(data_digest)
+}
+
+##' Return the current hashes recorded to the data digest directory generated during the last
+##' build.
 ##'
 ##' @title dpr_data_digest
-##' @param path path to data package 
+##' @param path path to data package
 ##' @return a data.frame showing the hashes found in the data digest
 ##' @author jmtaylor
 ##' @export 
 dpr_data_digest <- function(path="."){
-  ddig <- list.files(
-    file.path(path, dpr_yaml_get(path)$data_digest_directory),
-    full.names = TRUE
-  )
+  data_digest <- dpr_validate_data_digest_source(path)
   return(
     data.frame(
-      name = gsub("\\.rda_$", ".rda", basename(ddig)),
-      data_digest_hash = vapply(ddig, readLines, ""),
-      row.names = seq_along(ddig)
+      name = gsub("_$", "", basename(data_digest)),
+      data_digest_hash = vapply(data_digest, readLines, "", USE.NAMES = FALSE)
     )
   )
 }
@@ -128,32 +134,17 @@ dpr_data_hashes <- function(path="."){
   return(
     data.frame(
       name = basename(rda),
-      data_hash = vapply(rda, dpr_hash_file, ""),
-      row.names = seq_along(rda)
+      data_hash = dpr_hash_files(rda)
     )
   )
 }
 
-##' Get a checksum of an object from the git history.
-##'
-##' @title dpr_checksum_from_version
-##' @param version a string hash of a data version
-##' @param path a path to a data package
-##' @return a string of a checksum from an object recalled from the git history
-##' @author jmtaylor
-##' @export 
-dpr_checksum_from_version <- function(version, path = "."){
-  recall <- dpr_recall_data_version(version, path)
-  checksums <- list()
-  for(ck in names(recall))
-    checksums[[ck]] <- digest::digest(recall[[ck]], algo="md5")
-  return(checksums)
-}
-
-##' A data digest comparison table, comparing the current data digest with the current data hashes.
+##' A data digest comparison table, comparing the current data digest
+##' hashes with the current file hashes in the data directory.
 ##' 
-##' Compare the current hashes for the data listed in the data directory with the hashes listed in
-##' the data digest. The data digest is only updated when the package is built, not when it is
+##' Compare the current hashes for the data listed in the data
+##' directory with the hashes listed in the data digest. The data
+##' digest is only updated when the package is built, not when it is
 ##' simply rendered using `dpr_render()`.
 ##' @title dpr_compare_data_digest
 ##' @param path path to data package
@@ -163,11 +154,11 @@ dpr_checksum_from_version <- function(version, path = "."){
 ##' @export
 dpr_compare_data_digest <- function(path=".", display_digits = 7){
   comp <- merge(
-    dpr_data_hashes(path),
-    dpr_data_digest(path),
+    dpr_data_hashes(path), # the hashes of the files in the data directory
+    dpr_data_digest(path), # the hashes recorded to the data digest during the last build
     by = "name",
-    all.x = T,
-    all.y = T
+    all.x = TRUE,
+    all.y = TRUE
   )
 
   comp$same <-
@@ -180,11 +171,68 @@ dpr_compare_data_digest <- function(path=".", display_digits = 7){
   return(comp)
 }
 
+##' Private. Pulls blobs from the git objects database and returns
+##' them as an environments.
+##'
+##' @title dpr_git_hashes_to_envs
+##' @param hashes a character vector of full sha1 hashes
+##' @param path a path to a data package
+##' @return list of envs containing objects loaded from git history
+##' @author jmtaylor
+dpr_hashes_to_envs <- function(hashes, path = ".") {
+  envs <- lapply(hashes, function(hash){
+    tmp <- tempfile()
+    writeBin(
+      git2r::content(git2r::lookup(repo=path, sha=hash), raw=TRUE),
+      tmp
+    )
+    env <- new.env(parent = emptyenv())
+    load(tmp, envir = env)
+    unlink(tmp)
+    return(env)
+  })
+  return(envs)
+}
+
+##' Private. Return a vector of md5 checksums from a list of
+##' environments.
+##'
+##' @title dpr_envs_to_checksums
+##' @param envs environments to extact checksums
+##' @return singleton character vector 
+##' @author jmtaylor
+dpr_envs_to_checksums <- function(envs){
+  unlist(
+    lapply(envs, function(env){
+      nms <- ls(envir=env)
+      if(length(nms) > 1)
+        return("No checksum computed for rda files containing more than 1 object.")
+      return(digest::digest(get(nms, envir=env), algo="md5"))
+    })
+  )
+}
+
+##' Private. From a vector of hashes load the data and generate the
+##' checksum for the objects in memory.
+##'
+##' @title dpr_hashes_to_checksums
+##' @param hashes a character vector of full sha1 hashes
+##' @param path a path to a data package
+##' @return a character vector of checksums 
+##' @author jmtaylor
+dpr_hashes_to_checksums <- function(hashes, path){
+  dpr_envs_to_checksums(
+    dpr_hashes_to_envs(hashes, path)
+  )
+}
+
 ##' Return the history of all the former and current files in the `data` directory.
 ##'
 ##' @title dpr_data_history
 ##' @param path path to data package
-##' @param include_checksums a boolean value indicating if checksums should be included in the returned data.frame object; computing checksums is less performant
+##' @param include_checksums a boolean value indicating if checksums
+##'   should be included in the returned data.frame object; computing
+##'   checksums is less performant
 ##' @return a data.frame object
 ##' @author jmtaylor
 ##' @export
@@ -198,72 +246,56 @@ dpr_data_history <- function(path=".", include_checksums=FALSE){
   
   odb <- git2r::odb_blobs(path)
   odb <- odb[
-    odb$path == dpr_yaml_get(path)$data_directory,
-    c("sha", "path", "name", "author", "when")
+    odb$path == "data",
+    c("sha", "name", "author", "when")
   ]
+
+  if(nrow(odb) == 0)
+    stop("No files found at the `data` path commited to the git history.")
+
   names(odb)[names(odb) == "sha"] <- "blob_file_hash"
-  row.names(odb) <- 1:nrow(odb)
-  if(include_checksums){
-    cksum <- list()
-    for(hash in odb$blob_file_hash){
-      record <- dpr_checksum_from_version(hash, path)
-      cksum[[hash]] <- data.frame(
-        blob_file_hash = hash,
-        name = basename(names(record)),
-        object_md5 = unlist(record)
-      )
-    }
-    cksum <- do.call(rbind, cksum)
-    odb <- merge(odb, cksum) 
-  }
+  row.names(odb) <- 1:nrow(odb) # resetting the row names after subsetting
+
+  if(include_checksums)
+    odb$object_md5 <- dpr_hashes_to_checksums(odb$blob_file_hash, path)
+
   return(odb)
 }
 
 ##' Load a data version into memory by its hash. Only rda files in the data directory will be recalled.
 ##'
 ##' @title dpr_recall_data_version
-##' @param version the hash of the data to recall; partial hashes allowed from 1 to n digits
+##' @param hashes the hashes of the data to recall; partial hashes allowed from 1 to 40 hexadecimal digits
 ##' @param path the path to the data package
 ##' @return a list of objects loaded from an rda file pulled from the git history
 ##' @author jmtaylor
 ##' @export 
-dpr_recall_data_version <- function(version, path = "."){
+dpr_recall_data_versions <- function(hashes, path = "."){
   dpr_is(path)
   if(!dpr_check_git(path))
     stop("Datapackage is not under git version control. Data can not be recalled.")
   odb <- git2r::odb_blobs(path)
+  
   rda <- unique(
-    odb[ grepl(paste0("^", version), odb$sha), c("name", "path", "sha") ]
-  ) 
-  rda <- rda[ rda$path == "data", ]
-  rda <- rda[ grepl("\\.[Rr][Dd][Aa]$", rda$name) ]
+    odb[
+      odb$path == "data" & 
+        grepl(paste0(paste0("^", hashes), collapse = "|"), odb$sha, ignore.case = TRUE) &
+        grepl("\\.rda$", odb$name, ignore.case = TRUE),
+      c("name", "sha")
+    ]
+  )
+    
   if( nrow(rda) == 0 )
     stop("Data version not found. Either version hash is not found, or the hash does not point to an `rda` file in the `data` directory.")
-  if( length(unique(rda$sha)) != 1 )
-    stop("Multiple hashes found from partial hash. Please use more digits.")
-  tmps <- file.path(tempdir(), rda$name)
 
   ## there can be multiple rdas with the same hash
   ## there can be multiple objects in each rda
-  ## this is a feature of rda files, but will it be a feature of rda files created by DPR2?
 
-  recall <- list()
-  tryCatch({
-    for(tmp in tmps){
-      writeBin(
-        git2r::content(git2r::lookup(repo=path, sha=version), raw=T),
-        tmp
-      )
-      env <- new.env(parent = emptyenv())
-      load(tmp, envir = env)
-      objs <- ls(envir = env)
-      out <- list()
-      ## using for loop to assign names
-      for(obj in objs)
-        out[[obj]] <- get(obj, envir = env)
-      recall[[tmp]] <- out
-    }
-  }, error = function(e) { rm(env); stop(e) }
+  objs <- lapply(
+      dpr_hashes_to_envs(rda$sha, path),
+      function(env) mget(ls(env), envir = env)
   )
-  return(recall)
+
+  names(objs) <- rda$name
+  return(objs)
 }
