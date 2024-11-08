455408c525b02224464d2d46f298a9bab36cf8fc
Dave Slager <slager@users.noreply.github.com>
2024-11-08 00:33:22 -0800

    helper fn for test path expectations on various OS

--- tests/testthat/test-path.R
+++ tests/testthat/test-path.R
@@ -1,6 +1,8 @@
 test_that("project_path functions work", {
+  # helper to normalize test expectations
+  nfp <- function(...) normalizePath(file.path(...), '/', FALSE)
   # normalize for equality tests later
-  pkg_dir <- normalizePath(tempfile(), '/', FALSE)
+  pkg_dir <- nfp(tempfile())
   dir.create(pkg_dir)
   on.exit(unlink(pkg_dir, recursive = TRUE))
   cwd <- getwd()
@@ -13,26 +15,26 @@ test_that("project_path functions work", {
   writeLines('Package: ', 'DESCRIPTION')
   # correct directory with no arguments, called from wd
   expect_equal(project_path(), pkg_dir)
-  expect_equal(project_data_path(), file.path(pkg_dir, 'data'))
-  expect_equal(project_extdata_path(), file.path(pkg_dir, 'inst', 'extdata'))
+  expect_equal(project_data_path(), nfp(pkg_dir, 'data'))
+  expect_equal(project_extdata_path(), nfp(pkg_dir, 'inst', 'extdata'))
   # now call from inside subfolder
   dir.create('subfolder')
   setwd('subfolder')
   # correct directory with no arguments
   expect_equal(project_path(), pkg_dir)
-  expect_equal(project_data_path(), file.path(pkg_dir, 'data'))
-  expect_equal(project_extdata_path(), file.path(pkg_dir, 'inst', 'extdata'))
+  expect_equal(project_data_path(), nfp(pkg_dir, 'data'))
+  expect_equal(project_extdata_path(), nfp(pkg_dir, 'inst', 'extdata'))
   # path with subfolder and file arguments
   expect_equal(
     project_path('hello', 'world.txt'),
-    file.path(pkg_dir, 'hello', 'world.txt')
+    nfp(pkg_dir, 'hello', 'world.txt')
   )
   expect_equal(
     project_data_path('hello', 'world.txt'),
-    file.path(pkg_dir, 'data', 'hello', 'world.txt')
+    nfp(pkg_dir, 'data', 'hello', 'world.txt')
   )
   expect_equal(
     project_extdata_path('hello', 'world.txt'),
-    file.path(pkg_dir, 'inst', 'extdata', 'hello', 'world.txt')
+    nfp(pkg_dir, 'inst', 'extdata', 'hello', 'world.txt')
   )
 })
