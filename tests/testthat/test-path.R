b3336dad821d274fa20c9be341291aeefe85b390
Dave Slager <slager@users.noreply.github.com>
2024-11-08 08:07:21 -0800

    tweaks

--- tests/testthat/test-path.R
+++ tests/testthat/test-path.R
@@ -1,5 +1,6 @@
 test_that("project_path functions work", {
   pkg_dir <- tempfile()
+  pkg_name <- basename(pkg_dir)
   dir.create(pkg_dir)
   on.exit(unlink(pkg_dir, recursive = TRUE))
   cwd <- getwd()
@@ -11,20 +12,22 @@ test_that("project_path functions work", {
   expect_error(project_extdata_path())
   writeLines('Package: ', 'DESCRIPTION')
   # correct directory with no arguments, called from wd
-  expect_equal(basename(project_path()), basename(pkg_dir))
+  expect_equal(basename(project_path()), pkg_name)
   expect_equal(basename(project_data_path()), 'data')
+  expect_equal(basename(dirname(project_data_path())), pkg_name)
   expect_equal(basename(project_extdata_path()), 'extdata')
-  # now call from inside subfolder
-  dir.create('subfolder')
-  setwd('subfolder')
+  expect_equal(basename(dirname(project_extdata_path())), 'inst')
+  expect_equal(basename(dirname(dirname(project_extdata_path()))), pkg_name)
+  # test that we still find package root when getwd() is within a subfolder
+  dir.create('subfolder777')
+  setwd('subfolder777')
   # correct directory with no arguments
-  expect_equal(basename(project_path()), basename(pkg_dir))
-  expect_equal(basename(project_data_path()), 'data')
+  expect_equal(basename(project_path()), pkg_name)
+  expect_false(grepl('subfolder777', project_path()))
   # path with subfolder and file arguments
   pp <- project_path('hello', 'world.txt')
+  expect_false(grepl('subfolder777', pp))
   expect_equal(basename(pp), 'world.txt')
   expect_equal(basename(dirname(pp)), 'hello')
-  expect_equal(basename(dirname(dirname(pp))), basename(pkg_dir))
-  pdp <- project_data_path('hello', 'world.txt')
-  expect_equal(basename(dirname(pdp)), 'hello')
+  expect_equal(basename(dirname(dirname(pp))), pkg_name)
 })
