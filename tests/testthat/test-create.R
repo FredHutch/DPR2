a11d1d97ffb012ceda1ef8e885a1ec3c03288be6
Dave Slager <slager@users.noreply.github.com>
2024-11-20 10:21:22 -0800

    avoid partial matching

--- tests/testthat/test-create.R
+++ tests/testthat/test-create.R
@@ -122,7 +122,7 @@ test_that("no dpr_(description_)_init warning about default package name", {
   twd <- tempfile()
   dir.create(twd)
   setwd(twd)
-  expect_no_warning(dpr_init(renv = FALSE))
+  expect_no_warning(dpr_init(renv_init = FALSE))
 })
 
 cleanup(tdir)
