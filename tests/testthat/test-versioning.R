b9b37e712d5691ab7ff2389f068b17714cb4e423
Jason Taylor <jmtaylor@fredhutch.org>
2025-01-23 12:39:48 -0800

    Revert hash test changes
    This reverts commit 1a61e9ae9ba0c03c058ab1fe2dc927cc333c80e8.

--- tests/testthat/test-versioning.R
+++ tests/testthat/test-versioning.R
@@ -1,15 +1,21 @@
 ## check defaulting to data_digest when git is not used in the repository
 testthat::test_that("check dpr_hash against git2r hash", {
 
+  path <- "setup.R"
+  expect_true(
+    dpr_hash_files(path) ==
+      system(paste("git hash-object", path), intern = T)
+  )
+
   temp <- tempfile()
-  system(paste("git cat-file -p c9f3063f8011e0c01c54bf2ce6921453a5fe5f03 >", temp))
+  saveRDS(1:10, temp)
   expect_true(
     dpr_hash_files(temp) ==
       system(paste("git hash-object", temp), intern = T)
   )
 
   temp <- tempfile()
-  system(paste("git cat-file -p 90bcc0954510a4576598172f4eef6960cb03a4d6 >", temp))
+  saveRDS(letters[rep(1:26,1e4)], temp)
   expect_true(
     dpr_hash_files(temp) ==
       system(paste("git hash-object", temp), intern = T)
