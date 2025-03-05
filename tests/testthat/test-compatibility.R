74941d68aa47cf7a58353d5950e0d30ca0971dd6
Jason Taylor <jmtaylor@fredhutch.org>
2025-03-05 07:06:01 -0800

    update tests

--- tests/testthat/test-compatibility.R
+++ tests/testthat/test-compatibility.R
@@ -1,24 +1,64 @@
 testthat::test_that("checking DataPackageR compatibility functions", {
 
-  file.copy(test_path("dpr1package"), tempdir(), recursive = TRUE)
-  path <- file.path(tempdir(), "dpr1package/")
+  tdir <- getPkgDir()
 
-  expect_warning(
-    dpr_build(path),
+  file.copy(test_path("dpr1package"), tdir, recursive = TRUE)
+  path1 <- file.path(tdir, "dpr1package/")
+
+  expect_error(
+    dpr_build(path1),
     "Rendering a data package using DPR2 when the yaml is from DataPackageR"
   )
 
-  ## convert DataPackageR yaml to DPR2 yaml
-  dpr_dpr1_yaml_convert(path)
-
-  expect_false(
-    dpr_is_dpr1(path)
+  expect_error(
+    dpr_render(path1),
+    "Rendering a data package using DPR2 when the yaml is from DataPackageR"
   )
+
+  expect_error(
+    dpr_yaml_get(path1),
+    "Attemping to load a yaml of a DataPackageR package"
+  )
+
+  pkgn <- "testPkg"
+  path2 <- file.path(tdir, pkgn)
+  createPkg(tdir, pkgn, list(renv_init = FALSE))
+
+  expect_error(
+    dpr_convert(path2),
+    "not detected as DataPackageR package"
+  )
+
+  expect_warning(
+    dpr_convert(path1, renv_init = FALSE),
+    "`datapackager.yml` was found, skipping creating that file."
+  )
+
+  yml <- yaml::read_yaml(file.path(path1, "datapackager.yml"))
   expect_true(
-    dpr_is_dpr2(path)
+    !is.null(yml$process_directory) && yml$process_directory == "data-raw"
   )
 
-  dpr_build(path)
-  unlink(path)
+  expect_true(
+    all(
+      yml$process_on_build == c("mtcars.R", "iris.R", "trees.Rmd")
+    )
+  )
+
+  expect_true(
+    length(list.files(path1, "DATADIGEST")) == 0
+  )
+
+  expect_true(
+    length(list.files(path1, "NEWS.md")) == 0
+  )
+
+  expect_true(
+    length(list.files(file.path(path1, yml$data_digest_directory))) == 3
+  )
+
+  dpr_build(path1)
+
+  cleanup(tdir)
 
 })
