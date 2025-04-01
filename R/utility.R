95a0b375f6877535be1df2151f3ad09154ddf258
Jason Taylor <jmtaylor@fredhutch.org>
2025-04-01 10:13:05 -0700

    move dpr_path to utility

--- R/utility.R
+++ R/utility.R
@@ -205,3 +205,12 @@ dpr_save <- function(objects, path = dpr_path(), envir = parent.frame()){
   }
   invisible(objects)
 }
+
+#' Construct path from data package directory
+#'
+#' @param ... Trailing path components passed to `file.path()`. All arguments must
+#'   be the same length or length one.
+#' @return The normalized path with the additional path components appended.
+#'   Throws an error if no root is found.
+#' @export
+dpr_path <- function(...) rprojroot::find_package_root_file(...)
