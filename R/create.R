2849c8220304de1540e8dd9546155334afe72d37
Jason Taylor <jmtaylor@fredhutch.org>
2025-07-23 08:43:12 -0700

    fix spelling typos

--- R/create.R
+++ R/create.R
@@ -72,7 +72,7 @@ dpr_get_template <- function(regex){
 #' * `render_on_build`       : `TRUE`, whether to render the processing scripts when the package is built or not
 #' * `render_env_mode`       : `isolate`, valid values are "isolate" or "share" which determine if each processing script is run in its own R session (isolate) or the same R session (share). No option allows processing scripts to be run in the current session.
 #' * `write_to_vignettes`    : `TRUE`, if vignettes should be generated when rendering
-#' * `write_data_docs`       : `TRUE`, if data documentation should be generated when whendering
+#' * `write_data_docs`       : `TRUE`, if data documentation should be generated when rendering
 #' * `to_build_directory`    : `inst/to_build`, the location of the tracking directory for what script are built and objects are saved.
 #' * `build_tarball`         : `FALSE`, if a package tarball should be built when the package is built
 #' * `install_on_build`      : `FALSE`, if the package should be installed when the package is built
@@ -81,7 +81,7 @@ dpr_get_template <- function(regex){
 #' There are also optional values can will be used but are not required.
 #'
 #' * `process_on_build`       : a character vector of which processing scripts to run from the `process_directory` location when the package is rendered. These overwrite what scripts have been assigned in `inst/to_build/scripts` with `dpr_add_scripts`.
-#' * `objects`                : which objecsts to save from the processing scripts environmetnts. These values overwrite what objects have been assigned in `inst/to_build/objects` with `dpr_add_objects`.
+#' * `objects`                : which objects to save from the processing scripts environments. These values overwrite what objects have been assigned in `inst/to_build/objects` with `dpr_add_objects`.
 #' * `r_session_wait_timeout` : How long should a callr session wait before timing out. Add this to `datapackager.yml` if `dpr_build` or `dpr_render` return timeout errors. Units are in milliseconds with the default being 3000. A value of 5000 or higher will most likely prevent timeout errors.
 #
 #' @title dpr_yaml_defaults
