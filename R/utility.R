dd1bc3eb19a245c734bbe074840816c25c7d939b
Dave Slager <slager@users.noreply.github.com>
2025-09-19 15:43:55 -0700

    rm 2nd copy of dpr_path() and redocument (#124)

--- R/utility.R
+++ R/utility.R
@@ -413,12 +413,3 @@ dpr_rm_scripts <- function(scripts, path = "."){
 dpr_rm_objects <- function(objects, path = "."){
   dpr_rm_added(objects, "objects", path)
 }
-
-#' Construct path from data package directory
-#'
-#' @param ... Trailing path components passed to `file.path()`. All arguments must
-#'   be the same length or length one.
-#' @return The normalized path with the additional path components appended.
-#'   Throws an error if no root is found.
-#' @export
-dpr_path <- function(...) rprojroot::find_package_root_file(...)
